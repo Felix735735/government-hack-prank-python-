85b157cdb9952a70c1a49d472307ed32c45b0519
Felix <83507487+Felix735735@users.noreply.github.com>
2021-06-22 17:27:30 +0100

    Create prank.py

---
+++ prank.py
@@ -0,0 +1,44 @@
+#This is a government hack piece of code
+#Initialising
+#20%
+#40%
+#60#
+#100#
+
+government = input("What government do you want to hack? (example: British, Chinese)")
+database = input("What database do you want to hack?")
+time = input("What time do you want to start this hack?")
+project = input("What will this project be called?")
+name = ("And lastly, what is your name?")
+
+
+print(" The " + government + " has been hacked,the " + database + "database has been hacked,this project will take place at " + time + " .This project will be called " + project + " .This has been executed by " + name)
+
+
+import time
+time.sleep(0.75)
+print("STARTING")
+time.sleep(2)
+print("INTIALISING")
+time.sleep(1)
+print("10%")
+time.sleep(3)
+print("70%")
+time.sleep(1)
+print("100%")
+time.sleep(0.3)
+print("Completed!")
+
+time.sleep(0.1)
+print("NOT A SCAM, PLEASE READ FULL SENTENCE!      PLEASE VERIFY YOUR NOT A ROBOT BY COMPLETING THIS NUMBER SUM1")
+
+from turtle import *
+
+color('green')
+speed(11)
+for i in range(120):
+    circle(i * 1.5)
+    right(4)
+
+hideturtle()
+done()
